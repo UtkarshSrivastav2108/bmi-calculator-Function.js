c1e55ea7caf951d13c66c41de47a9391c19c378b
Utkarsh Srivastav <71812037+UtkarshSrivastav2108@users.noreply.github.com>
2021-01-15 18:12:11 +0530

    Update bmi calculator Function.js

--- bmi calculator Function.js
+++ bmi calculator Function.js
@@ -5,3 +5,16 @@ function bmiCalculator(weight,height){
     }
     var bmi=bmiCalculator(65,1.8);
     console.log(bmi);
+
+
+
+
+// ------------------------------square root
+
+function square_root(s) {
+var calculator= (Math.sqrt(s))
+return calculator;
+
+}
+var bmi= square_root(81);
+console.log(bmi);
